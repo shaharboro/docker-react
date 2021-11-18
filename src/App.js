d4ebaee7b56acbc7d1747cd982e71314e244e028
shaharboroc <shahar@trackimo.com>
2021-11-18 06:05:37 +0200

    build attempt1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          sb there
+          shahar there
         </p>
         <a
           className="App-link"
