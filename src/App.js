58f6e19a82749e36cae10119346c7d606f99f57b
shaharboroc <shahar@trackimo.com>
2021-11-19 16:21:07 +0200

    dev initial

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          xxx there
+          dev branch here
         </p>
         <a
           className="App-link"
